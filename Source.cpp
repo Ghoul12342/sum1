397b799cbc1c64d57e1b752caffb5022a7f7041c
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-11-12 18:07:35 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "russian");
+	//вариант 2
+	//задание 1
+	//const int n = 12;
+	//int arr[n];
+	//int i;
+	//srand(time(nullptr));
+	//for (int i = 1; i < n; i++) {
+	//	arr[i] = rand() % 11 - 5;
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+	//int sum = 0;
+	//for (int i = 1; i < n; i++) {
+	//	sum = sum + arr[i];
+	//}
+	//cout << "сумма = " << sum << endl;
+	//задание 2
+	//const int n = 13;
+	//int arr[n];
+	//int i;
+	//srand(time(nullptr));
+	//for (int i = 1; i < n; i++) {
+	//	arr[i] = rand() % 25 - 12;
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+	//int pr = 1;
+	//for (int i = 1; i < n; i++) {
+	//	if (arr[i] < 0) {
+	//		pr = pr * arr[i];
+	//	}
+	//}
+	//cout << "pr = " << pr << endl;
+	//задание 3
+	//const int n = 11;
+	//int arr[n];
+	//int i;
+	//srand(time(nullptr));
+	//for (int i = 1; i < n; i++) {
+	//	arr[i] = rand() % 21 - 0;
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+	//int k = 0;
+	//for (int i = 1; i < n; i++) {
+	//	if ((arr[i] % 3 == 0) or (arr[i] % 5 == 0) or (arr[i] % 7 == 0)) {
+	//		k++;
+	//	}
+	//}
+	//cout << "k = " << k << endl;
+	//задание 4
+	//const int n = 15;
+	//int arr[n];
+	//int i;
+	//srand(time(nullptr));
+	//for (int i = 1; i < n; i++) {
+	//	arr[i] = rand() % 26 - 0;
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+	//int sum = 0;
+	//for (int i = 1; i < n; i++) {
+	//	if (arr[i] % 2 != 0) {
+	//		sum = sum + arr[i];
+	//	}
+	//}
+	//cout << "sum = " << sum << endl;
+}
